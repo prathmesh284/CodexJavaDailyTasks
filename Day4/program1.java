bef71ab114cddd7e593740cb13b253e4a5369c7b
Pratham <jadhavprathmesh284@gmail.com>
2024-08-29 22:09:12 +0530

    Day 4 of core java daily tasks

---
+++ Day4/program1.java
@@ -0,0 +1,9 @@
+class Codex{
+    public static void main(String[] args) {
+        for(char i=65;i<74;i++){
+            if(i%2==1){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
